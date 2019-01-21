3dd233d1a8c23fb5abbbd4163fe78bd2840d3e5a
Alden <aj.molina092@gmail.com>
2019-01-20 19:00:49 -0500

    Added 2 new story states. Able to go through states with keycodes.

--- Assets/State.cs
+++ Assets/State.cs
@@ -6,10 +6,15 @@ using UnityEngine.UI;
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
 {
-   [TextArea(10,14)] [SerializeField] string storyText;
+    [TextArea(10,14)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
 
     public string GetStateStory()
     {
         return storyText;
+    }
+    public State[] GetNextStates()
+    {
+        return nextStates;
     }
 }
